f1d0c12ffd695a7ec75a5824ecf8ef2ea896bb1a
lazarzlatic <lazar.zlatic@gmail.com>
2023-09-24 13:58:09 +0200

    Pydantic Input validation, swagger configuration

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Fast API
+
+## API, CRUD operations
+get, post, put, delete
+
+## Pydantic
+Input validation, swagger configuration
--- books2.py
+++ books2.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Body
 from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 
@@ -20,12 +21,22 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    id: int
+    id: Optional[int] = None
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
 
+    class Config:
+        json_schema_extra = {
+            'example': {
+                'title': 'A new book',
+                'author': 'codingwithR',
+                'description': 'A new description of a book',
+                'rating': 5
+
+            }
+        }
 
 BOOKS = [
     Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book', 5),
@@ -46,7 +57,13 @@ async def read_all_books():
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
-    BOOKS.append(new_book)
+    BOOKS.append(find_book_id(new_book))
+
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+
+    return book
 
 
 
